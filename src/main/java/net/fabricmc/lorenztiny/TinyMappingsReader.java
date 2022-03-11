bbe97671f5b4ee6f40d715bb6abf6b9b11c082e6
calmilamsy <bumbill00@gmail.com>
2022-03-11 15:29:47 +0000

    License date

--- src/main/java/net/fabricmc/lorenztiny/TinyMappingsReader.java
+++ src/main/java/net/fabricmc/lorenztiny/TinyMappingsReader.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of lorenz-tiny, licensed under the MIT License (MIT).
  *
- * Copyright (c) 2020 FabricMC
+ * Copyright (c) 2020, 2022 FabricMC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
